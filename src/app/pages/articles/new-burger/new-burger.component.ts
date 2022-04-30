1deb9fc5e3771a0a7c1ef57db5084c9284d60066
Saint-Pierre <saint-pierre.kassi@ism.edu.sn>
2022-04-30 23:16:07 +0000

    init

---
+++ src/app/pages/articles/new-burger/new-burger.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-burger',
+  templateUrl: './new-burger.component.html',
+  styleUrls: ['./new-burger.component.css']
+})
+export class NewBurgerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
